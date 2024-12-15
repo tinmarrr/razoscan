a62361451185d2655a20ea7195afd89ea5ca9e1b
tinmarrr <martin.girardin@sciencespo.fr>
2024-12-15 15:52:51 +0100

    Add files via upload

---
+++ src/components/features25.js
@@ -0,0 +1,162 @@
+import React, { useState, Fragment } from 'react'
+
+import PropTypes from 'prop-types'
+
+import './features25.css'
+
+const Features25 = (props) => {
+  const [activeTab, setActiveTab] = useState(0)
+  return (
+    <div className="thq-section-padding">
+      <div className="features25-container2 thq-section-max-width">
+        <div className="features25-tabs-menu">
+          <div
+            onClick={() => setActiveTab(0)}
+            className="features25-tab-horizontal1"
+          >
+            <div className="features25-divider-container1">
+              {activeTab === 0 && <div className="features25-container3"></div>}
+            </div>
+            <div className="features25-content1">
+              <h2 className="thq-heading-2">
+                {props.feature1Title ?? (
+                  <Fragment>
+                    <span className="features25-text4">
+                      Customizable Templates
+                    </span>
+                  </Fragment>
+                )}
+              </h2>
+              <span className="thq-body-small">
+                {props.feature1Description ?? (
+                  <Fragment>
+                    <span className="features25-text2">
+                      Easily customize and edit templates to suit your specific
+                      IT document needs
+                    </span>
+                  </Fragment>
+                )}
+              </span>
+            </div>
+          </div>
+          <div
+            onClick={() => setActiveTab(1)}
+            className="features25-tab-horizontal2"
+          >
+            <div className="features25-divider-container2">
+              {activeTab === 1 && <div className="features25-container4"></div>}
+            </div>
+            <div className="features25-content2">
+              <h2 className="thq-heading-2">
+                {props.feature2Title ?? (
+                  <Fragment>
+                    <span className="features25-text3">
+                      Wide Variety of Documents
+                    </span>
+                  </Fragment>
+                )}
+              </h2>
+              <span className="thq-body-small">
+                {props.feature2Description ?? (
+                  <Fragment>
+                    <span className="features25-text5">
+                      Choose from a wide range of IT document templates
+                      including policies, procedures, forms, and more
+                    </span>
+                  </Fragment>
+                )}
+              </span>
+            </div>
+          </div>
+          <div
+            onClick={() => setActiveTab(2)}
+            className="features25-tab-horizontal3"
+          >
+            <div className="features25-divider-container3">
+              {activeTab === 2 && <div className="features25-container5"></div>}
+            </div>
+            <div className="features25-content3">
+              <h2 className="thq-heading-2">
+                {props.feature3Title ?? (
+                  <Fragment>
+                    <span className="features25-text6">
+                      Professional Look and Feel
+                    </span>
+                  </Fragment>
+                )}
+              </h2>
+              <span className="thq-body-small">
+                {props.feature3Description ?? (
+                  <Fragment>
+                    <span className="features25-text1">
+                      Get professionally designed templates that give your IT
+                      documents a polished look
+                    </span>
+                  </Fragment>
+                )}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="features25-image-container">
+          {activeTab === 0 && (
+            <img
+              alt={props.feature1ImgAlt}
+              src={props.feature1ImgSrc}
+              className="features25-image1 thq-img-ratio-16-9"
+            />
+          )}
+          {activeTab === 1 && (
+            <img
+              alt={props.feature2ImgAlt}
+              src={props.feature2ImgSrc}
+              className="features25-image2 thq-img-ratio-16-9"
+            />
+          )}
+          {activeTab === 2 && (
+            <img
+              alt={props.feature3ImgAlt}
+              src={props.feature3ImgSrc}
+              className="features25-image3 thq-img-ratio-16-9"
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Features25.defaultProps = {
+  feature3Description: undefined,
+  feature1ImgAlt: 'Customizable Templates Image Alt',
+  feature1Description: undefined,
+  feature3ImgAlt: 'Professional Look and Feel Image Alt',
+  feature2Title: undefined,
+  feature1Title: undefined,
+  feature1ImgSrc:
+    'https://images.unsplash.com/photo-1690983329777-62a5b0ae912d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTczNDI3NDA4OXw&ixlib=rb-4.0.3&q=80&w=1080',
+  feature3ImgSrc:
+    'https://images.unsplash.com/photo-1573495613349-ae44f9b25d37?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTczNDI3NDA4OXw&ixlib=rb-4.0.3&q=80&w=1080',
+  feature2Description: undefined,
+  feature3Title: undefined,
+  feature2ImgAlt: 'Wide Variety of Documents Image Alt',
+  feature2ImgSrc:
+    'https://images.unsplash.com/photo-1587449156852-c191c00da9b3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTczNDI3NDA4OXw&ixlib=rb-4.0.3&q=80&w=1080',
+}
+
+Features25.propTypes = {
+  feature3Description: PropTypes.element,
+  feature1ImgAlt: PropTypes.string,
+  feature1Description: PropTypes.element,
+  feature3ImgAlt: PropTypes.string,
+  feature2Title: PropTypes.element,
+  feature1Title: PropTypes.element,
+  feature1ImgSrc: PropTypes.string,
+  feature3ImgSrc: PropTypes.string,
+  feature2Description: PropTypes.element,
+  feature3Title: PropTypes.element,
+  feature2ImgAlt: PropTypes.string,
+  feature2ImgSrc: PropTypes.string,
+}
+
+export default Features25
